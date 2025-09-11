4e7cee57232989c6fb9082fe626054448b873065
IcicleStars <ice.randellesalvador@gmail.com>
2025-09-11 10:12:59 -0700

    Slight naming convention change to flooding header

--- project skeleton code copy/includes/FloodingHdr.h
+++ project skeleton code copy/includes/FloodingHdr.h
@@ -1,12 +1,12 @@
-#ifndef __FLOODING_H__
-#define __FLOODING_H__
+#ifndef __FLOODING_HDR_H__
+#define __FLOODING_HDR_H__
 
-typedef struct FloodingHeader{ 
+typedef struct FloodingHdr{ 
     am_addr_t source;       // Flooding Origin Address
     uint16_t seq_num;       // Sequence Number
     uint8_t ttl;            // time to live
 
     uint8_t payload[0];     // end header with zero length array to access payload
-} FloodingHeader;
+} FloodingHdr;
 
 #endif
